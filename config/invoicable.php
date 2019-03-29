8af3b47cdc22ae68f0cdc97f56e679b7f47290d8
rabol <steen@rabol.com>
2019-03-29 12:51:03 +0100

    Wrong definition in the config file

--- config/invoicable.php
+++ config/invoicable.php
@@ -4,5 +4,5 @@ return [
     'default_currency' => 'EUR',
     'default_status' => 'concept',
     'locale' => 'nl_NL',
-    'invoice_reference_generator' => '\SanderVanHooft\Invoicable\InvoiceReferenceGenerator::class'
+    'invoice_reference_generator' => SanderVanHooft\Invoicable\InvoiceReferenceGenerator::class
 ];
